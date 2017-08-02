b291cc8387d86b3d26b4743ea030a01b1b76c66c
Siddharth Bharat Purohit <siddharthbharatpurohit@gmail.com>
2017-08-02 17:37:37 +0530

    make display portable

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    if(!qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "1")) {
+        qDebug() << "could not set env variable ";
+    }
     QApplication a(argc, argv);
     profiled_designer w;
     w.show();
